fd3a859c7afc8072366cdfd4733f89d30f112900
Hayden Roche <haydenroche5@gmail.com>
2020-03-20 20:32:23 -0500

    changes to rescale pp

--- rocheml/preprocessing/rescalepreprocessor.py
+++ rocheml/preprocessing/rescalepreprocessor.py
@@ -2,5 +2,5 @@ class RescalePreprocessor:
     def __init__(self, divisor):
         self.divisor = divisor
 
-    def preprocess(self, image):
-        return image / (self.divisor * 1.0)
+    def preprocess(self, imgs):
+        return [img / (self.divisor * 1.0) for img in imgs]
