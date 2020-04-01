809434dfe3ccc108c4455271364c4f7471695641
Hayden Roche <haydenroche5@gmail.com>
2020-04-01 17:49:59 -0500

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='rocheml',
-    version='1.0',
+    version='1.1',
     author='Hayden Roche',
     author_email='haydenroche5@gmail.com',
     description='A package of useful machine learning classes and functions.',
