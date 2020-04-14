146c36620840cddd2b47f564f4219b1697eb466c
Hayden Roche <haydenroche5@gmail.com>
2020-04-14 14:24:37 -0500

    change setup.py to reflect new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='rocheml',
-    version='1.1',
+    version='1.2',
     author='Hayden Roche',
     author_email='haydenroche5@gmail.com',
     description='A package of useful machine learning classes and functions.',
