456a061f77e212567981a00f2a22cb59e4c9dbc7
Hayden Roche <haydenroche5@gmail.com>
2020-04-30 21:08:37 -0500

    make croppreprocessor work with PIL images and numpy arrays

--- rocheml/preprocessing/croppreprocessor.py
+++ rocheml/preprocessing/croppreprocessor.py
@@ -1,3 +1,6 @@
+import PIL
+
+
 class CropPreprocessor:
     def __init__(self, box_origin, box_width, box_height):
         self.box_origin = box_origin
@@ -8,9 +11,14 @@ class CropPreprocessor:
         crops = []
 
         for img in imgs:
-            crop = img[self.box_origin[1]:self.box_origin[1] +
-                       self.box_height, self.box_origin[0]:self.box_origin[0] +
-                       self.box_width]
+            if isinstance(img, PIL.Image.Image):
+                crop = img.crop(box=(self.box_origin[0], self.box_origin[1],
+                                     self.box_origin[0] + self.box_width,
+                                     self.box_origin[1] + self.box_height))
+            else:
+                crop = img[self.box_origin[1]:self.box_origin[1] +
+                           self.box_height, self.
+                           box_origin[0]:self.box_origin[0] + self.box_width]
             crops.append(crop)
 
         return crops
