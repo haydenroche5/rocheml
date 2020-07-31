f861c4f53b0f1b30d8a90584276e25267761d845
Hayden Roche <haydenroche5@gmail.com>
2020-07-31 14:30:59 -0500

    make contrast preprocessor adaptive

--- rocheml/preprocessing/contrastpreprocessor.py
+++ rocheml/preprocessing/contrastpreprocessor.py
@@ -1,13 +1,33 @@
 import cv2
+import numpy as np
 
 
 class ContrastPreprocessor:
-    def __init__(self, alpha, beta):
+    def __init__(self, alpha, beta, adaptive=False, threshold=0):
         self.alpha = alpha
         self.beta = beta
+        self.adaptive = adaptive
+        self.threshold = threshold
 
     def preprocess(self, imgs):
-        return [
-            cv2.convertScaleAbs(img, alpha=self.alpha, beta=self.beta)
-            for img in imgs
-        ]
+        preprocessed_imgs = []
+
+        if self.adapative:
+            for img in imgs:
+                gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+                brightness = np.mean(gray_img)
+
+                if brightness < self.threshold:
+                    preprocessed_imgs.append(
+                        cv2.convertScaleAbs(img,
+                                            alpha=self.alpha,
+                                            beta=self.beta))
+                else:
+                    preprocessed_imgs.append(img)
+        else:
+            preprocessed_imgs = [
+                cv2.convertScaleAbs(img, alpha=self.alpha, beta=self.beta)
+                for img in imgs
+            ]
+
+        return preprocessed_imgs
